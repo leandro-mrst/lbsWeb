7d6a3701bc3f4cc565415b7ad584118c24a63478
leandro-mrst <leandromrssantos@gmail.com>
2025-07-09 15:41:33 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Propedia`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
