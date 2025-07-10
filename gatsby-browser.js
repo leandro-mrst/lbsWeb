ff129e2277ed9a623145dce6b9ab7373f4c1c63b
leandro-mrst <leandromrssantos@gmail.com>
2025-07-10 17:08:01 -0300

    adicionei a aba de info

---
+++ gatsby-browser.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDatabase, faFont, faBacon, faAtom } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faDatabase, faFont, faBacon, faAtom);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
-import * as React from 'react'; // React is correctly imported 
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS imported
-import { Link } from 'gatsby'; // Link component from Gatsby imported
+import * as React from 'react'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Link } from 'gatsby'; 
 import { StaticImage } from 'gatsby-plugin-image'
 import './style.css';
 import MenuBar from '../components/layout.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-// Step 2: Define your component
+
+
 const IndexPage = () => {
   return (
     <div>
@@ -33,8 +35,11 @@ const IndexPage = () => {
  
               <h1 className="display-4 fw-bold mb-3">A database of peptide-protein interactions</h1>
 
-              <p className="lead mb-4">
-                PROPEDIA is a database of peptide-protein complexes, clustered in three methodologies: based on **peptide sequences**, based on **structure interface**, and based on **binding sites**. PROPEDIA main goal is to give new insights into peptide design of biotechnological interests.
+              <p className="mb-8">
+                PROPEDIA is a database of peptide-protein complexes, clustered in three methodologies: 
+                based on <strong>peptide sequences</strong>, based on <strong>structure interface</strong>, 
+                and based on <strong>binding sites</strong>. 
+                PROPEDIA main goal is to give new insights into peptide design of biotechnological interests.
               </p>
 
   
@@ -46,12 +51,64 @@ const IndexPage = () => {
           </div>
         </div>
       </section>
+
+       <div id='info' className='container py-5'>
+      <div className='row justify-content-center align-items-center g-4'>
+
+        <div className='col-12 col-md-6 col-lg-3 text-success'>
+      <div className='info-box p-4'> 
+        <FontAwesomeIcon icon={["fas", "database"]} className='icon' /> 
+        <div className='text-content'>
+          <div className='number mb-1'>~49,300</div> 
+          <div className='divider'></div>
+          <div className='description text-success'>PEPTIDE COMPLEXES</div>
+        </div>
+      </div>
+    </div>
+
+    <div className='col-12 col-md-6 col-lg-3 text-success'>
+      <div className='info-box p-4'>
+        <FontAwesomeIcon icon={["fas", "font"]} className='icon' />
+        <div className='text-content'>
+          <div className='number mb-1'>~1,845</div>
+          <div className='divider'></div>
+          <div className='description text-success'>SEQUENCE CLUSTERS</div>
+        </div>
+      </div>
+    </div>
+
+    <div className='col-12 col-md-6 col-lg-3 text-success'>
+      <div className='info-box p-4'>
+        <FontAwesomeIcon icon={["fas", "bacon"]} className='icon' />
+        <div className='text-content'>
+          <div className='number mb-1'>~1,891</div>
+          <div className='divider'></div>
+          <div className='description text-success'><u>INTERFACE CLUSTERS</u></div>
+        </div>
+      </div>
+    </div>
+
+    <div className='col-12 col-md-6 col-lg-3 text-success'>
+      <div className='info-box p-4'>
+        <FontAwesomeIcon icon={["fas", "atom"]} className='icon' />
+        <div className='text-content'>
+          <div className='number mb-1'>~1,466</div>
+          <div className='divider'></div>
+          <div className='description text-success'>BINDING SITE CLUSTERS</div>
+        </div>
+      </div>
+    </div>
+      </div>
+
+      <p className='text-end mt-4 text-muted small'>*Last updated on: Nov 2022</p>
+
+    </div>
       </main>
     </div>
   );
 };
-// You'll learn about this in the next task, just copy it for now
+
 export const Head = () => <title>Propedia</title>
 
-// Step 3: Export your component
+
 export default IndexPage
